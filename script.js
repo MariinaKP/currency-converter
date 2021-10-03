8197e91a09eae69c980568ad12281d89a57038eb
MariinaKP <pironevamarina@gmail.com>
2021-10-03 15:15:20 +0300

    Added screenshot

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ function convert() {
     let amount = $("#amount").val();
     let selectedOne = $('#currency__one--options').find(":selected").val();
     let selectedTwo = $('#currency__two--options').find(":selected").val();
-    console.log(selectedOne)
     let result;
 
     $.ajax({
         url: 'https://v6.exchangerate-api.com/v6/26327dac634a5e3a936c5305/latest/' + selectedOne,
         dataType: 'json',
         success: function(content) {
-            result = (content.conversion_rates[selectedTwo] * amount).toFixed(2) + " " + selectedTwo ;
-            $('#result').value = result;
+            result = (content.conversion_rates[selectedTwo] * amount).toFixed(2) + " " + selectedTwo;
+            $('#result').val(result);
         }
     });
 }
